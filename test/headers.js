c4c6e9fdf68287add60471613fb781c43c3360d6
Yarik Ponomarenko <yarik.ponomarenko@gmail.com>
2014-12-01 00:26:04 +0200

    refactored test/headers.js

--- test/headers.js
+++ test/headers.js
@@ -1,32 +1,8 @@
-var assert = require("assert"),
-    utils = require("./helpers.js"),
-    getEtalon = utils.getEtalon,
-    getResult = utils.getResult,
-    har2ammo = utils.har2ammo;
+var test = require("./helpers").test;
 
 describe('Headers', function () {
 
-    it('Headers off', function () {
-        var test = "ya.ru.customHeaders.off",
-            etalon = getEtalon(test),
-            config = utils.getConfig(test + '.config'),
-            input = utils.getHar('ya.ru'),
-            output = utils.getOut(test),
-            exec = har2ammo(config + input + output),
-            toTest = getResult(test);
+    it('Headers off', test('ya.ru.customHeaders.off'));
 
-        assert.equal(etalon, toTest);
-    })
-
-    it('Headers off', function () {
-        var test = "ya.ru.customHeaders",
-            etalon = getEtalon(test),
-            config = utils.getConfig(test + '.config'),
-            input = utils.getHar('ya.ru'),
-            output = utils.getOut(test),
-            exec = har2ammo(config + input + output),
-            toTest = getResult(test);
-
-        assert.equal(etalon, toTest);
-    })
+    it('Headers off', test('ya.ru.customHeaders'));
 });
